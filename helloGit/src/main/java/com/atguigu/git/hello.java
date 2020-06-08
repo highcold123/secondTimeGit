824ac856bdfc6e615778d6bd1eaaf9c6f8e36f89
hicold <gh2855522@163.com>
2020-06-08 19:34:27 +0800

    创建分支

--- helloGit/src/main/java/com/atguigu/git/hello.java
+++ helloGit/src/main/java/com/atguigu/git/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("这是我第二次操作Git！");
         System.out.println("更新1！");
         System.out.println("更新2！");
+        System.out.println("创建分支！");
     }
 }
