f33d6cdd62750a03a4d37159e1db9c1a755e616a
hicold <gh2855522@163.com>
2020-06-08 19:23:13 +0800

    更新1

--- helloGit/src/main/java/com/atguigu/git/hello.java
+++ helloGit/src/main/java/com/atguigu/git/hello.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class hello {
     public static void main(String[] args){
         System.out.println("这是我第二次操作Git！");
+        System.out.println("更新1！");
+        System.out.println("更新2！");
     }
 }
